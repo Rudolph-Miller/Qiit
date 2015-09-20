7ea340848f17825b99beaaac7252cb0856a3541d
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-09-20 18:51:14 +0900

    Use ListView

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@
 var React = require('react-native');
 var {
   Image,
+  ListView,
   StyleSheet,
   Text,
   View,
@@ -13,7 +14,7 @@ var DATA = require('../data.json');
 module.exports = React.createClass({
   getInitialState: function() {
     return {
-      items: null
+      items: []
     };
   },
 
@@ -21,36 +22,63 @@ module.exports = React.createClass({
     this.setState({items: DATA});
   },
 
-  render: function() {
-    var content;
-
-    if (this.state.items) {
-      content = this.state.items.map(function(item) {
-        return(
-          <View>
-            <Text>
-              {item.title}
-            </Text>
-          </View>
-        );
-      });
-    } else {
-      content = (<Text>Loading...</Text>);
-    }
-
-    return (
-      <View style={styles.cotainer}>
-        {{content}}
+  renderItem: function(item) {
+    return(
+      <View style={styles.container}>
+        <Image
+          source={{uri: item.user.profile_image_url}}
+          style={styles.thubnail} />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.userId}>{item.user.id}</Text>
+        </View>
       </View>
+    );
+  },
+
+  dataSource: function() {
+    return (
+      new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }).cloneWithRows(this.state.items)
+    );
+  },
+
+  render: function() {
+    return (
+      <ListView style={styles.listView}
+        dataSource={this.dataSource()}
+        renderRow={this.renderItem}
+      />
     );
   }
 });
 
 var styles = StyleSheet.create({
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
+  },
+  thubnail: {
+    width: 53,
+    height: 81,
+  },
+  rightContainer: {
+    flex: 1
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  userId: {
+    textAlign: 'center'
   }
 });
