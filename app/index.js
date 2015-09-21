84485b23df689bf813dfa7521844ed77e1d11cea
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-09-22 01:11:25 +0900

    Add fetch function

--- app/index.js
+++ app/index.js
@@ -14,21 +14,44 @@ var Loading = require('./loading');
 var Content = require('./content');
 var Items = require('./items');
 
-var DATA = require('../data.json');
+var _navigator;
 
 module.exports = React.createClass({
   getInitialState: function() {
     return {
       items: [],
+      error: null,
       loaded: false
     };
   },
 
+  getRequestURL: {
+    items: function() {
+      return 'https://qiita.com/api/v2/items';
+
+    }
+  },
+
+  fetchData: function() {
+    var route = _navigator.getCurrentRoutes()[0];
+    fetch(this.getRequestURL[route.name].call(this))
+      .then(function(res) {
+        return res.json();
+      }).then(function(json) {
+        this.setState({
+          items: json,
+          loaded: true
+        });
+      }.bind(this)).catch(function(e) {
+        this.setState({
+          error: e,
+          loaded: true
+        });
+      }.bind(this));
+  },
+
   componentDidMount: function() {
-    this.setState({
-      items: DATA,
-      loaded: true
-    });
+    this.fetchData();
   },
 
   renderMain: {
@@ -70,8 +93,12 @@ module.exports = React.createClass({
   },
 
   renderScene: function(route, navigator) {
+    _navigator = navigator;
     route.title = this.getTitle[route.name].call(this, (route));
     var main = this.renderMain[route.name].call(this, route, navigator);
+    if (this.state.error) {
+      main = (<Text>{this.state.error}</Text>);
+    }
     return (
       <ScrollView>
         <Toolbar 
