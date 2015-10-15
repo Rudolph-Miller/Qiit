61fefefc568044c1220d059d00ea2a4c79031f05
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-10-15 10:20:30 +0900

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -16,16 +16,7 @@ var Qiit = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text>Qiit</Text>
       </View>
     );
   }
@@ -37,17 +28,7 @@ var styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('Qiit', () => Qiit);
